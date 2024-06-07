54fb851626108749a679688f40d03064098820c4
AmerBecarevic <99793620+AmerBecarevic@users.noreply.github.com>
2024-06-07 14:07:13 +0200

    Update ControllerCupkica.ino

--- CONTROLLER/ControllerCupkica.ino
+++ CONTROLLER/ControllerCupkica.ino
@@ -10,6 +10,7 @@
 #define BI2 25
 
 int brzina = 180; // Speed setting
+int delayTime = 50;
 
 const int kanal1 = 0;
 const int kanal2 = 1;
@@ -17,7 +18,8 @@ const int kanal2 = 1;
 // LM393 sensor pins
 #define SENSOR_LEFT_PIN 34
 #define SENSOR_RIGHT_PIN 35
-bool a1 = false, a2 = false, b1 = false, b2 = false;
+
+bool a1 = false, a2 = false, b1 = false, b2 = false, mode = true;
 
 
 // this header is needed for Bluetooth Serial -> works ONLY on ESP32
@@ -52,12 +54,192 @@ void setup() {
   ledcAttachPin(PWMB, kanal2);
 }
 
+void RC(char button)
+{
+  
+  switch (button) {
+    case 'F':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_1, value);
+      //Serial.println("btn 1");
+      a1 = true;
+      a2 = false;
+      b1 = true;
+      b2 = false;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina);
+      Serial.print("A1: "); Serial.println(a1);
+      Serial.print("A2: "); Serial.println(a2);
+      Serial.print("B1: "); Serial.println(b1);
+      Serial.print("B2: "); Serial.println(b2);
+      break;
+    case 'R':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_2, value);
+      //Serial.println("btn 2");
+      
+      a1 = false;
+      a2 = true;
+      b1 = true;
+      b2 = false;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina);
+      break;
+    case 'L':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+      
+      a1 = true;
+      a2 = false;
+      b1 = false;
+      b2 = true;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina);
+      break;
+    case 'B':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+      
+      a1 = false;
+      a2 = true;
+      b1 = false;
+      b2 = true;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina);
+      break;
+    case 'S':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+      
+      a1 = false;
+      a2 = false;
+      b1 = false;
+      b2 = false;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina);
+      break;
+    case 'G':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+
+      a1 = true;
+      a2 = false;
+      b1 = true;
+      b2 = false;
+
+      ledcWrite(kanal1, brzina + brzina / 3);
+      ledcWrite(kanal2, brzina);
+      break;
+    case 'I':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+      
+      a1 = true;
+      a2 = false;
+      b1 = true;
+      b2 = false;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina + brzina / 3);
+      break;
+    case 'H':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+
+      a1 = false;
+      a2 = true;
+      b1 = false;
+      b2 = true;
+
+      ledcWrite(kanal1, brzina + brzina / 3);
+      ledcWrite(kanal2, brzina);
+      break;
+    case 'J':  
+      Serial.print("Button: "); Serial.println(button);
+      //digitalWrite(led_pin_3, value);
+      //Serial.println("btn 3");
+
+      a1 = false;
+      a2 = true;
+      b1 = false;
+      b2 = true;
+
+      ledcWrite(kanal1, brzina);
+      ledcWrite(kanal2, brzina + brzina / 3);
+      break;
+  }
+}
+
+void Lajna()
+{
+  
+  int sensorLeftValue = digitalRead(SENSOR_LEFT_PIN);
+  int sensorRightValue = digitalRead(SENSOR_RIGHT_PIN);
+  Serial.print("Button: "); Serial.println(sensorLeftValue);
+  Serial.print("Button: "); Serial.println(sensorRightValue);
+
+
+  if (sensorLeftValue == LOW && sensorRightValue == LOW) {
+    // Both sensors on black line, move forward
+    a1 = true;
+    a2 = false;
+    b1 = true;
+    b2 = false;
+    //ledcWrite(kanal1, brzina);
+    //ledcWrite(kanal2, brzina);
+  } else if (sensorLeftValue == HIGH && sensorRightValue == LOW) {
+    // Left sensor off line, turn slightly right
+    a1 = false;
+    a2 = true;
+    b1 = true;
+    b2 = false;
+    //ledcWrite(kanal1, brzina + 10);
+    //ledcWrite(kanal2, brzina);
+  } else if (sensorLeftValue == LOW && sensorRightValue == HIGH) {
+    // Right sensor off line, turn slightly left
+    a1 = true;
+    a2 = false;
+    b1 = false;
+    b2 = true;
+    //ledcWrite(kanal1, brzina);
+    //ledcWrite(kanal2, brzina  + 10);
+  } else {
+    // Both sensors off line, stop or take corrective action
+    // For simplicity, let's stop the motors
+    a1 = false;
+    a2 = false;
+    b1 = false;
+    b2 = false;
+    //ledcWrite(kanal1, brzina);
+    //ledcWrite(kanal2, brzina);
+  }
+
+  // Set motor speed
+  ledcWrite(kanal1, brzina);
+  ledcWrite(kanal2, brzina);
+}
 
 void loop() {
+  /*
   Serial.print("A1:                   "); Serial.println(a1);
   Serial.print("A2:                      "); Serial.println(a2);
   Serial.print("B1:                         "); Serial.println(b1);
   Serial.print("B2:                           "); Serial.println(b2);
+  */
+
+  //Serial.print("Button: "); Serial.println(ESP_BT.read());
+  
   
   if(a1)
     digitalWrite(AIN1, HIGH);
@@ -75,6 +257,23 @@ void loop() {
     digitalWrite(BI2, HIGH);
   if(!b2)
     digitalWrite(BI2, LOW);
+
+  incoming = ESP_BT.read(); //Read what we receive 
+  char button = incoming;
+
+  if(mode)
+    {
+      delayTime = 50;
+      brzina = 180;
+      RC(button);
+    }
+    else
+    {
+      delayTime = 100;
+      brzina = 140;
+      Lajna();
+    }
+  
   
   // -------------------- Receive Bluetooth signal ----------------------
   if (ESP_BT.available()) 
@@ -82,136 +281,21 @@ void loop() {
     incoming = ESP_BT.read(); //Read what we receive 
 
     // separate button ID from button value -> button ID is 10, 20, 30, etc, value is 1 or 0
-    char button = incoming;
+    button = incoming;
     
-    switch (button) {
-      case 'F':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_1, value);
-        //Serial.println("btn 1");
-        a1 = true;
-        a2 = false;
-        b1 = true;
-        b2 = false;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina);
-        Serial.print("A1: "); Serial.println(a1);
-        Serial.print("A2: "); Serial.println(a2);
-        Serial.print("B1: "); Serial.println(b1);
-        Serial.print("B2: "); Serial.println(b2);
-        break;
-      case 'R':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_2, value);
-        //Serial.println("btn 2");
-        
-        a1 = false;
-        a2 = true;
-        b1 = true;
-        b2 = false;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina);
-        break;
-      case 'L':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-        
-        a1 = true;
-        a2 = false;
-        b1 = false;
-        b2 = true;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina);
-        break;
-      case 'B':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-        
-        a1 = false;
-        a2 = true;
-        b1 = false;
-        b2 = true;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina);
-        break;
-      case 'S':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-        
-        a1 = false;
-        a2 = false;
-        b1 = false;
-        b2 = false;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina);
-        break;
-      case 'G':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-
-        a1 = true;
-        a2 = false;
-        b1 = true;
-        b2 = false;
-
-        ledcWrite(kanal1, brzina + brzina / 3);
-        ledcWrite(kanal2, brzina);
-        break;
-      case 'I':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-        
-        a1 = true;
-        a2 = false;
-        b1 = true;
-        b2 = false;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina + brzina / 3);
-        break;
-      case 'H':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-
-        a1 = false;
-        a2 = true;
-        b1 = false;
-        b2 = true;
-
-        ledcWrite(kanal1, brzina + brzina / 3);
-        ledcWrite(kanal2, brzina);
-        break;
-      case 'J':  
-        Serial.print("Button: "); Serial.println(button);
-        //digitalWrite(led_pin_3, value);
-        //Serial.println("btn 3");
-
-        a1 = false;
-        a2 = true;
-        b1 = false;
-        b2 = true;
-
-        ledcWrite(kanal1, brzina);
-        ledcWrite(kanal2, brzina + brzina / 3);
-        break;
+    if(button == 'M')
+    {
+      mode = !mode;
+      a1 = false;
+      a2 = false;
+      b1 = false;
+      b2 = false;
     }
+
+    
   }
 
-  // Set motor speed
-  
-  delay(100);
+  delay(delayTime);
 
 }
-
 
